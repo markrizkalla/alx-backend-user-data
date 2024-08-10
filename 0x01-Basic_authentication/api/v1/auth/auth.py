745b3b6ff4539abf83a102632c5aa5a0c64e5f42
markrizkalla <mark.rizkalla1@gmail.com>
2024-08-10 05:15:42 +0300

    doc

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -8,14 +8,33 @@ class Auth():
     """ Auth class """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ require auth"""
+        """ require auth
+        Args"
+        path: str
+        excluded_paths: excluded path
+
+        Return: bool
+        """
 
         return False
 
     def authorization_header(self, request=None) -> str:
-        """ auth header """
+        """ auth header 
+
+        Args:
+        request: the request
+
+        Return:
+        str
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """ current user """
+        """ current user 
+        Args:
+        request: the req
+
+        Return:
+        user
+        """
         return None
